6d8df226253d59d2511eb8adf64b594fdde8b4f5
Faruq Bakare <bakarefaruq.a@gmail.com>
2020-12-03 12:13:46 +0100

    styling channelrow

--- src/ChannelRow.js
+++ src/ChannelRow.js
@@ -1,5 +1,6 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
+import VerifiedIcon from "@material-ui/icons/CheckCircleOutline";
 import "./ChannelRow.css";
 
 function ChannelRow({
@@ -12,8 +13,18 @@ function ChannelRow({
 }) {
   return (
     <div className="channelRow">
-      <Avatar src={image}>{channel[0]}</Avatar>
-      <h2>ChannelRow</h2>
+      <Avatar className="channelRow__logo" src={image}>
+        {channel[0]}
+      </Avatar>
+      <div className="channelRow__text">
+        <h4>
+          {channel} {verified && <VerifiedIcon />}
+        </h4>
+        <p>
+          {subs} subscribers . {noOfVideos} videos
+        </p>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
